e9d24c4d505a95ced470199302a6d7a5fba5c6d0
caitberry <cberry1882@gmail.com>
2024-03-29 13:50:24 -0600

    created file to house all functions for this work

---
+++ Functions.R
@@ -0,0 +1,26 @@
+# Title: Functions Module
+# File name: Functions.R
+# Description: This file contains all of the functions relevant to 
+#   calculating the spectral estimate and allan variance of time series data
+#   possibly with gaps
+
+
+
+# libraries #############################################
+
+
+# Functions #############################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
+
+## function 1 ###########################################
