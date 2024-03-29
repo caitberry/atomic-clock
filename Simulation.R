9c1974fd331e26992b31e1497283ceb3d1bf52e5
caitberry <cberry1882@gmail.com>
2024-03-29 13:52:50 -0600

    created file to house code to run simulation of known noise processes

---
+++ Simulation.R
@@ -0,0 +1,7 @@
+# Title: Simulation Study
+# File name: Simulation.R
+# Description: This file contains the code required to run simulations of
+#  known noise processes in order to compare spectral-based AVAR and OAVAR
+
+
+# libraries #############################################
