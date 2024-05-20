15d429ca545cb26a6d315540ff56bcfb363b161c
aak3 <aak3@tethys.bitl.nist.gov>
2024-05-20 15:31:10 -0600

    updates on real data analysis

--- Code/dryRunAnalysis.R
+++ Code/dryRunAnalysis.R
@@ -15,32 +15,6 @@ library(RSpectra) #eigenvalue solving
 
 mtse=modules::use("Functions.R")
 
-###############################################################################################
-###############################################################################################
-dat2024_04_24_ErYb_AlSr <- read.csv("Data/2024-04-24-ErYb-AlSr.csv",header = F,sep="",na.strings = "NaN")
-dim(dat2024_04_24_ErYb_AlSr)
-colnames(dat2024_04_24_ErYb_AlSr)=c("MJD","FracDiff")
-dat2024_04_24_ErYb_AlSr$seconds=1:dim(dat2024_04_24_ErYb_AlSr)[1]
-
-
-# source("Functions.R")
-dat=dat2024_04_24_ErYb_AlSr%>%mutate(missing=is.na(FracDiff),
-                                     date=convertToDateTime(MJD, origin = "1858-11-17",tz="MDT"))
-
-
-dat=filter(dat, missing==F)
-t.vec <- dat$seconds
-x.t=dat$FracDiff
-N=length(x.t)
-
-test=spectralEstWithUnc(x.t = x.t,t.vec=t.vec,N.fourier = floor(N/2) + 1,#100,
-                        numTapers = 10,calcCov = F,
-                        myW = 4/N*3)
-test$V.mat$e.values
-V.mat <- get_tapers(t.vec, W = 4/N*3, K = 9) 
-V.mat$e.values
-
-
 
 ### data analysis steps
 # 1. read in data
@@ -50,9 +24,116 @@ V.mat$e.values
 # can start with 8/N or 12/N for W
 # 4. Look at data, spectral estimate, and tapers
 # 5. calculate avar estimate with spectrum for a series of tau values
-
 # 6. calculate avar estimate using old method for same tau series
 # 7. plot both with uncertainties
+
+
+
+###############################################################################################
+###############################################################################################
+
+# 1. read in data
+dat2024_04_24_ErYb_AlSr <- read.csv("Data/2024-04-24-ErYb-AlSr.csv",header = F,sep="",na.strings = "NaN")
+dim(dat2024_04_24_ErYb_AlSr)
+colnames(dat2024_04_24_ErYb_AlSr)=c("MJD","FracDiff")
+
+plot(dat2024_04_24_ErYb_AlSr$MJD,dat2024_04_24_ErYb_AlSr$FracDiff)
+dataInDF=data.frame(MJD=dat2024_04_24_ErYb_AlSr$MJD,y=dat2024_04_24_ErYb_AlSr$FracDiff-mean(dat2024_04_24_ErYb_AlSr$FracDiff,na.rm = T))
+
+ggplot(dataInDF,aes(MJD,y))+
+  geom_point()
+
+ggplot(dat2024_04_24_ErYb_AlSr,aes(MJD,FracDiff))+
+  geom_point()
+
+dat2024_04_24_ErYb_AlSr$seconds=1:dim(dat2024_04_24_ErYb_AlSr)[1]
+
+
+dat=dat2024_04_24_ErYb_AlSr%>%mutate(missing=is.na(FracDiff),
+                                     date=convertToDateTime(MJD, origin = "1858-11-17",tz="MDT"))
+
+plot(dat$MJD,dat$FracDiff)
+
+dat=filter(dat, missing==F)
+t.vec <- dat$seconds
+x.t=dat$FracDiff#-mean(dat$FracDiff)
+N=length(x.t)
+
+# 2. concatenate small gaps
+# ????????
+
+# 3. get spectral estimate
+# 3a) try a variety of W and K values and check eigenvalues to make sure you've made good selections
+# can start with 8/N or 12/N for W
+V.mat <- mtse$get_tapers(t.vec, W = 4/N*3, K = 10) 
+V.mat$e.values
+test=mtse$spectralEstWithUnc(x.t = x.t,t.vec=t.vec,N.fourier = floor(N/2) + 1,#100,
+                        numTapers = 10,calcCov = F,
+                        myW = 4/N*3)
+test$V.mat$e.values
+
+# 4. Look at data, spectral estimate, and tapers
+library(RColorBrewer)
+# Define the number of colors you want
+nb.cols <- 10+1
+mycolors <- colorRampPalette(brewer.pal(8, "Accent"))(nb.cols)
+
+datDF=data.frame(x.t=x.t-mean(x.t),t.vec=t.vec)
+ggplot(datDF,aes(t.vec,x.t))+
+  geom_point()
+  
+resDF=data.frame(ratio="test",
+                n.fourier=length(test$freq),
+                freq=test$freq,
+                spectrum=test$spec.hat)
+
+ggplot(resDF,aes(freq,spectrum,col=factor(interaction(n.fourier))))+
+  geom_line()+
+  scale_y_log10()+
+  scale_x_log10()
+
+## look at tapers
+
+taperDF=data.frame(V.mat$tapers)
+colnames(taperDF)=c("1","2","3","4","5","6","7","8","9","10")
+taperDF$t=t.vec
+
+dataDF=data.frame(t=t.vec,
+                  value=x.t)
+
+alltData=data.frame(t=min(taperDF$t):max(taperDF$t))
+
+taperDF=merge(alltData,taperDF,by="t",all.x = T)
+
+dataDF=merge(alltData,dataDF,by="t",all.x = T)
+dataDF$type="Data"
+dataDF$Taper="Data"
+
+taperDFlong=melt(taperDF,id.vars = "t",variable.name = "Taper")
+taperDFlong$type="Tapers"
+
+allTaperDat=bind_rows(taperDFlong,dataDF)
+
+ggplot(allTaperDat,aes(t,value,col=Taper))+
+  geom_line()+
+  facet_wrap(~type,nrow = 2,scales = "free_y",
+             strip.position = "left", 
+             labeller = as_labeller(c(Data = "Clock Ratio Data", Tapers = "Tapers") ) )+
+  # scale_color_brewer(palette = "Accent")+
+  scale_color_manual(values=mycolors)+
+  # theme(axis.title.y=element_blank())+
+  theme(legend.position = "none",legend.title=element_blank(),strip.background = element_blank(),
+        strip.placement = "outside")+
+  ylab(NULL)
+
+
+# 5. calculate avar estimate with spectrum for a series of tau values
+
+# 6. calculate avar estimate using old method for same tau series
+
+# 7. plot both with uncertainties
+
+
 
 # 
 # 
