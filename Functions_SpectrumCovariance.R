d24fbcccca56ac70a2711ddf7953722d08042b0e
aak3 <aak3@tethys.bitl.nist.gov>
2024-10-25 07:00:35 -0600

    file to calculate the spectrum with parallel covariance estimation, fits into previous functions

---
+++ Functions_SpectrumCovariance.R
@@ -0,0 +1,182 @@
+
+library(parallel)
+library(Rcpp)
+library(RcppArmadillo)
+
+## Spectral Estimate and Uncertainty ################################################
+#inputs:  (->) x.t = vector of data (possibly with NA values)
+#         (->) t.vec
+#         (->) N.fourier
+#         (->) numTapers = number of tapers
+#         (->) calcCov = True if you want to calculate the covariance matrix
+#         (->) myW = Analysis half-bandwidth
+#         (->) isWhite
+#         (->) acf.lag
+#output:  (<-) spectral estimate with covariance matrix
+
+spectralEstWithUnc <- function(x.t,t.vec,N.fourier,numTapers,calcCov=T,myW,isWhite = TRUE,acf.lag=4,numCores){
+  N <- length(t.vec)
+  freq <- seq(0,0.5, length.out = N.fourier)
+  
+  delta.f <- freq[2] #interval spacing between frequencies, needed for spectral avar calculation
+  
+  ##calculate tapers for this data spacing
+  V.mat <- mtse$get_tapers(t.vec, W = myW, K = numTapers) #W was 4/N
+  
+  MTSE_full <- mtse$MT_spectralEstimate(x.t, freq, V.mat$tapers) 
+  
+  Cov.mat=NA
+  ### calculate the covariance matrix 
+  if(calcCov==T){
+    Cov.mat=spec_cov.mat(x.t, t.vec, N.fourier, V.mat$tapers, isWhite,acf.lag,numCores)
+  }
+  
+  return(list(freq=freq,
+              spec.hat=MTSE_full$spectrum,Cov.mat=Cov.mat))
+  
+}
+
+#########################################################
+### start of new parallel cov Functions
+#########################################################
+
+## upper triangle indices
+generate_upper_triangle_indices <- function(N) {
+  
+  print("generate_upper_triangle_indices")
+  # Create a sequence of row indices
+  row_indices <- rep(1:N, each = N)
+  # Create a sequence of column indices
+  col_indices <- rep(1:N, times = N)
+  # Combine row and column indices into a matrix
+  all_indices <- cbind(row_indices, col_indices)
+  # Filter out indices to get only the upper triangle (excluding the diagonal)
+  upper_triangle_indices <- all_indices[row_indices < col_indices, ]
+  return(upper_triangle_indices)
+}
+
+## compute the entry C.mat_ij
+compute_entry_parallel <- function(ij, tapMat=taperMatrix, sett.vec=t.vec, setfreq=freq,setK = K, setN = N, c = c_vec){
+  #i,j, taperMat = taperMatrix, setK = K, setN = N, c = c_vec
+  print("compute_entry_parallel")
+  
+  i = ij[1]
+  j = ij[2]
+  
+  V_star <- t(tapMat*exp(1i*2*pi*setfreq[i]*sett.vec))
+  V <- tapMat*exp(-1i*2*pi*setfreq[j]*sett.vec)
+  return(est_entry_FFT(V_star, V, c, setK, setN))
+}
+
+## fill in the upper triangle of a matrix from a vector
+fill_upper_triangle <- function(vec, N, incl_diag = TRUE) {
+  
+  print("fill_upper_triangle")
+  # Initialize an NxN matrix with NA
+  mat <- matrix(NA, nrow = N, ncol = N)
+  
+  # Get the indices of the upper triangle (excluding the diagonal)
+  upper_tri_indices1 <- which(upper.tri(mat, diag = incl_diag), arr.ind = TRUE)
+  upper_tri_indices <- upper_tri_indices1[order(upper_tri_indices1[,1]),]
+  
+  # Check if the length of the vector matches the number of upper triangle elements
+  if (length(vec) != nrow(upper_tri_indices)) {
+    stop("Length of vector does not match the number of upper triangle elements excluding the diagonal.")
+  }
+  
+  # Fill the matrix upper triangle with the elements of the vector
+  mat[upper_tri_indices] <- vec
+  
+  return(mat)
+}
+
+spec_cov.mat=function(x.t, t.vec,N.fourier,taperMat,isWhite,
+                      max.lag.acf,numCores){
+  
+  
+  print("spec_cov.mat")
+  N <- length(stats::na.omit(x.t)) #length of data without gaps
+  taperMatrix=taperMat
+  K <- dim(taperMatrix)[2] 
+  
+  freq <- seq(0, 0.5, length.out = N.fourier)
+  
+  ### make the cluster
+  cl <- parallel::makeCluster(numCores)
+  
+  ## Pre-processing ####
+  
+  ### 1. load packages ####
+  parallel::clusterEvalQ(cl, {
+    library(Rcpp)
+  })
+  
+  ### 2. send source C code ####
+  parallel::clusterEvalQ(cl, {
+    Rcpp::sourceCpp('/home/aak3/NIST/atomic-clock/CovarianceCalculation/est_entry_FFT.cpp')
+    # Rcpp::sourceCpp(paste(folderLocation,'CovarianceCalculation/est_entry_FFT.cpp',sep=""))
+  })
+  
+  ### 3. Calculate predetermined variables ####
+  
+  #### c vector
+  print("c vector")
+  if(isWhite){
+    sample.acf <- c(1,rep(0,max.lag.acf-1))
+  }
+  if(!isWhite){
+    sample.acf <- stats::acf(x.t, plot=FALSE, lag.max=max.lag.acf,na.action = stats::na.exclude)$acf
+  }
+  
+  c_vec <- c(sample.acf,rep(0, times = N-max.lag.acf), 0, rep(0, times = N-max.lag.acf), rev(sample.acf[-1]))
+  
+  #### list of indices (don't really need to send this to the cluster, but good for debugging if needed)
+  upper_triangle_indices <- generate_upper_triangle_indices(N.fourier)
+  
+  ### 4. Send variables/necessary functions to the cluster ####
+  print("K is")
+  print(K)
+  print("N is")
+  print(N)
+  parallel::clusterExport(cl, list("t.vec", "N", "K", "c_vec", "taperMatrix", "freq", 
+                                   "upper_triangle_indices"),envir=environment()) #objects
+  
+  print("exported the objects")
+  parallel::clusterExport(cl, "compute_entry_parallel") #functions
+  
+  ## Run code on cluster ####
+  
+  ##### (run this code chunk all at once ###
+  #####   for most accurate timing)      ###
+  start_time_fast = Sys.time() #for timing, start time
+  print("start parallel")
+  my_list <- list() #a vector to hold the entries
+  my_list <- parallel::parApply(cl, FUN = compute_entry_parallel, X = upper_triangle_indices, MARGIN = 1)
+  
+  total_time_fast = Sys.time() - start_time_fast
+  print(total_time_fast) 
+  
+  parallel::stopCluster(cl) #stop the cluster
+  #######--------------------------------###
+  
+  
+  
+  ## Make the Covariance matrix ####
+  ### 1. Fill in upper triangle of C.mat ####
+  C.mat <- fill_upper_triangle(vec = my_list, N.fourier, incl_diag = FALSE)
+  
+  ### 2. Fill in lower triangle of C.mat ####
+  C.mat[lower.tri(C.mat)] <- t(C.mat)[lower.tri(C.mat)]
+  
+  ### 3. Fill in Diagonal of C.mat ####
+  tN = max.lag.acf
+  R_mat <- stats::toeplitz(c(seq(1,0.1, length.out = tN), rep(0, times = N-tN))) #to start
+  diag(C.mat) <- norm(t(taperMatrix)%*%R_mat%*%taperMatrix/K, type = "2")
+  
+  ## look at result ##
+  return(list(C.mat = C.mat))
+}
+#########################################################
+### end of new parallel cov Functions
+#########################################################
+
