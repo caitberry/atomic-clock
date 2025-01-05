b6ad801cfd3453a98ffa115946205f55dec7b870
Koepke <aak3@NIST.GOV>
2025-01-05 13:24:53 -0700

    lots of plots, deleting some

--- Code/SimulatedData_CovarianceTest_manyReps.R
+++ Code/SimulatedData_CovarianceTest_manyReps.R
@@ -1,7 +1,8 @@
 library(tidyr)
 library(ggplot2)
+library(dplyr)
 
-numberOfSimulations = 100
+numberOfSimulations = 500
 
 set.seed(123)
 N=2048+2048*.5 #length of data with gaps
@@ -58,12 +59,12 @@ saveRDS(oamat,paste("Results/oamat",runDate,"_W",setWnum,"_K",setK,"_N",N,"_",nu
 
 ##tidy the data
 both.res.mat <- rbind(oamat,samat)
-method_labels <- rep(c("avar", "spectral"), each = numberOfSimulations)
+method_labels <- rep(c("Current", "Spectral"), each = numberOfSimulations)
 df.messy <- as.data.frame(cbind(method_labels, both.res.mat))
-colnames(df.messy) <-c("method", taus)
+colnames(df.messy) <-c("Method", taus)
 
 
-dat <- df.messy %>% gather(tau, measurement, -method)
+dat <- df.messy %>% gather(tau, measurement, -Method)
 
 dat$measurement <- as.numeric(dat$measurement)
 dat$tau <- as.numeric(dat$tau)
@@ -73,9 +74,9 @@ minor_breaks <- rep(1:9,21 )*rep(10^(-10:10), each = 9)
 
 linedat_w=data.frame(tau=1:1000)
 linedat_w$truth=(1/linedat_w$tau)#+linedat$tau
-linedat_w$method="Truth"
+linedat_w$Method=NA
 
-ggplot(dat,aes(tau,measurement,col=method,group=interaction(tau,method)))+
+ggplot(dat,aes(tau,measurement,col=Method,group=interaction(tau,Method)))+
   geom_boxplot(lwd = 1.2)+
   ### add true straight line below
   # geom_abline(slope = -1,intercept = 0,size=1)+
@@ -89,7 +90,7 @@ ggplot(dat,aes(tau,measurement,col=method,group=interaction(tau,method)))+
   ### where to put legend. one option is "bottom", avoid having to place it. The tuple I have here
   ### basically specifies the x and y position in terms of the plot size in unit scale.
   theme_bw(base_size = 20)+
-  theme(legend.position = c(.15, .2))+
+  theme(legend.position = "inside", legend.position.inside =  c(.15, .2))+
   scale_y_log10(breaks = breaks, minor_breaks = minor_breaks)+
   scale_x_log10(breaks = breaks, minor_breaks = minor_breaks)+
   annotation_logticks()+
@@ -111,42 +112,118 @@ plot(MTSE_full$freqs,MTSE_full$spectrum)
 Cov.mat=spec_cov.mat(x.t = x.t, t.vec = t.vec, N.fourier = floor(N/2) + 1, 
                      taperMat = V.mat$tapers, isWhite = F,
                      max.lag.acf = 4,
-                     numCores=10)
+                     numCores=4)
 diag(Cov.mat)[1]
 specAVARest=mtse$AVAR_spec(spectral_est = MTSE_full$spectrum,taus = taus,calcUnc = T,Cov.mat = Cov.mat)
-# 
-# # 6. calculate avar estimate using old method for same tau series
-# 
-# plot(x.t)
-# 
-# oldAVARest=mtse$overlapping_avar_fn(y = na.omit(x.t),m = taus)
-# 
-# oldAVARestUncertainty=mtse$avar_CI(CI.level = .68,
-#                                    noise_type = "white noise", 
-#                                    avar_type = "chisquared", 
-#                                    avars = oldAVARest, 
-#                                    taus=taus,
-#                                    N=N)
-# 
-# # 7. plot both with uncertainties
-# 
-# 
-# avarDF=data.frame(avar=c(oldAVARest,specAVARest$avar),
-#                   avarLower=c(oldAVARestUncertainty$lower,specAVARest$avar-sqrt(specAVARest$avarVar)),
-#                   avarUpper=c(oldAVARestUncertainty$upper,specAVARest$avar+sqrt(specAVARest$avarVar)),
-#                   tau=rep(taus,2),
-#                   Method=rep(c("Current","Spectral"),each=length(taus)))
-# 
-# ggplot(avarDF,aes(tau,avar,col=Method,ymin=avarLower,ymax=avarUpper))+
-#   geom_point()+
-#   ### add true straight line below
-#   # geom_abline(slope = -1,intercept = 0,size=1)+
-#   # theme(legend.position = c(.15, .2))+
-#   scale_y_log10()+
-#   scale_x_log10()+
-#   annotation_logticks()+
-#   ylab(expression(sigma^2*(tau)))+
-#   xlab(expression(tau))+
-#   geom_errorbar()
-# # facet_wrap(~Ratio)
-# 
+oldAVARest=mtse$overlapping_avar_fn(y = na.omit(x.t),m = taus)
+
+oldAVARestUncertainty=mtse$avar_CI(CI.level = .68,
+                                   noise_type = "white noise",
+                                   avar_type = "chisquared",
+                                   avars = oldAVARest,
+                                   taus=taus,
+                                   N=N)
+
+avarDF=data.frame(avar=c(oldAVARest,specAVARest$avar),
+                  avarLower=c(oldAVARestUncertainty$lower,specAVARest$avar-sqrt(specAVARest$avarVar)),
+                  avarUpper=c(oldAVARestUncertainty$upper,specAVARest$avar+sqrt(specAVARest$avarVar)),
+                  tau=rep(taus,2),
+                  Method=rep(c("Current","Spectral"),each=length(taus)))
+
+ggplot(avarDF,aes(tau,avar,col=Method,ymin=avarLower,ymax=avarUpper))+
+  geom_point()+
+  ### add true straight line below
+  # geom_abline(slope = -1,intercept = 0,size=1)+
+  # theme(legend.position = c(.15, .2))+
+  scale_y_log10()+
+  scale_x_log10()+
+  annotation_logticks()+
+  ylab(expression(sigma^2*(tau)))+
+  xlab(expression(tau))+
+  geom_errorbar()
+# facet_wrap(~Ratio)
+
+
+
+
+ggplot(dat,aes(tau,measurement,col=Method,group=interaction(tau,Method)))+
+  geom_boxplot(lwd = 1.2)+
+  # geom_violin()+
+  scale_color_discrete(labels= c("avar"= "Avar","spectral"= "Spectral"),name="Method")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  theme_bw(base_size = 20)+
+  theme(legend.position = c(.15, .2))+
+  scale_y_log10(breaks = breaks, minor_breaks = minor_breaks)+
+  scale_x_log10(breaks = breaks, minor_breaks = minor_breaks)+
+  annotation_logticks()+
+  ylab(expression(sigma^2*(tau)))+
+  xlab(expression(tau))+
+  geom_line(data=linedat_w,aes(tau,truth,col=Method),linewidth = 1)+
+  geom_errorbar(data = avarDF, aes(tau, avar, col = "black", ymin = avarLower, ymax = avarUpper),
+                position = position_dodge(width = 0.25), width = 0.2, size = 0.8, linetype = "dashed", alpha = 0.7) # Error bars for avarDF
+
+
+linedat_w$lowerSim=NA
+linedat_w$upperSim=NA
+
+##############################################################################
+### Plotting the sim results and bars
+##############################################################################
+simBars= dat %>% group_by(tau,Method) %>% 
+  summarise(lowerSim=quantile(measurement,prob=c(.16)),
+            upperSim=quantile(measurement,prob=c(.84)),
+            mean=mean(measurement))
+
+ggplot(simBars,aes(tau,mean,ymin=lowerSim,ymax=upperSim,col=Method))+
+  geom_point()+
+  geom_errorbar()+
+  scale_color_discrete(labels= c("avar"= "Avar","spectral"= "Spectral"),name="Method")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  theme_bw(base_size = 20)+
+  theme(legend.position = c(.15, .2))+
+  scale_y_log10(breaks = breaks, minor_breaks = minor_breaks)+
+  scale_x_log10(breaks = breaks, minor_breaks = minor_breaks)+
+  annotation_logticks()+
+  ylab(expression(sigma^2*(tau)))+
+  xlab(expression(tau))+
+  geom_line(data=linedat_w,aes(tau,truth,col=Method),linewidth = 1)+
+  geom_errorbar(data=avarDF,aes(tau,avar,col=Method,ymin=avarLower,ymax=avarUpper))
+
+
+
+ggplot(simBars, aes(tau, mean, ymin = lowerSim, ymax = upperSim, col = Method)) +
+  geom_point(position = position_dodge(width = 0.1)) + # Offset points
+  geom_errorbar(position = position_dodge(width = 0.1), width = 0.2, size = 0.8) + # Error bars for simBars
+  scale_color_discrete(labels = c("avar" = "Avar", "spectral" = "Spectral"), name = "Method") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black")) +
+  theme_bw(base_size = 20) +
+  theme(legend.position = c(.15, .2)) +
+  scale_y_log10(breaks = breaks, minor_breaks = minor_breaks) +
+  scale_x_log10(breaks = breaks, minor_breaks = minor_breaks) +
+  annotation_logticks() +
+  ylab(expression(sigma^2 * (tau))) +
+  xlab(expression(tau)) +
+  geom_line(data = linedat_w, aes(tau, truth, col = Method), linewidth = 1) +
+  geom_errorbar(data = avarDF, aes(tau, avar, col = Method, ymin = avarLower, ymax = avarUpper),
+                position = position_dodge(width = 0.1), width = 0.2, size = 0.8, linetype = "dashed", alpha = 0.7) # Error bars for avarDF
+
+  
+
+ggplot(simBars, aes(tau, mean, ymin = lowerSim, ymax = upperSim, col = Method)) +
+  geom_point(position = position_dodge(width = 0.1)) + # Offset points
+  geom_errorbar(position = position_dodge(width = 0.1), width = 0.2, size = 0.8) + # Error bars for simBars
+  scale_color_discrete(labels = c("avar" = "Avar", "spectral" = "Spectral"), name = "Method") +
+  ylab(expression(sigma^2 * (tau))) +
+  xlab(expression(tau)) +
+  facet_wrap(~tau,scales = "free")+
+  geom_errorbar(data = avarDF, aes(tau, avar, col = Method, ymin = avarLower, ymax = avarUpper),
+                position = position_dodge(width = 0.1), width = 0.2, size = 0.8, linetype = "dashed", alpha = 0.7) # Error bars for avarDF
+
+
+
+
