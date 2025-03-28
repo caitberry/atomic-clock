0449b8d8d8446e9606f06bedbce75465d7172b43
smt3 <smt3@nist.gov>
2025-03-28 14:41:37 -0400

    update three-clock-imputation.py to visualize missingness, extract only is_good data, and find overlapping window of observation

--- st-interp/three_clocks/three-clock-imputation.py
+++ st-interp/three_clocks/three-clock-imputation.py
@@ -135,25 +135,56 @@ shift_data_Yb = open_shiftfile_Yb(path + "20240813_Yb_Freq_Shifts.txt")
 compute_nuSr_ErYb(data_ErYb)
 compute_nuYb_ErYb(data_ErYb)
 compute_nuAl_ErYb(data_ErYb)
+
+################################################################################
+## Visualize gaps in data 
+################################################################################
+## cd ~/anaconda3/envs/myenv
+## pip install missingno 
+
+import missingno as msno
+atts = ["MJD", "shift"]
+
+miss_inx_Al = pd.isnull(shift_data_Al["shift"])
+print(miss_inx_Al.sum())
+msno.matrix(shift_data_Al[atts])
+plt.show()
+
+miss_inx_Sr = pd.isnull(shift_data_Sr["shift"])
+print(miss_inx_Sr.sum())
+msno.matrix(shift_data_Sr[atts])
+plt.show()
+
+miss_inx_Yb = pd.isnull(shift_data_Yb["shift"])
+print(miss_inx_Yb.sum())
+msno.matrix(shift_data_Yb[atts])
+plt.show()
  
  
 ################################################################################
 #########################  Data Processing #####################################
-####                      Including finding common MDJ values               #####
 ################################################################################
+
+## Extract only "IS_GOOD" data for analysis------------------
+good_condition_al = shift_data_Al["IS_GOOD"] == 1
+shift_data_Al_good = shift_data_Al[good_condition_al].reset_index(drop=True)
+good_condition_sr = shift_data_Sr["IS_GOOD"] == 1
+shift_data_Sr_good = shift_data_Sr[good_condition_sr].reset_index(drop=True)
+good_condition_yb = shift_data_Yb["IS_GOOD"] == 1
+shift_data_Yb_good = shift_data_Yb[good_condition_yb].reset_index(drop=True)
+
+
+## Find common MDJ values--------------------------------
 
 #Change comb mjd type to float  
 common_mjd = data_ErYb["MJD"].astype(float)
 nuAl = data_ErYb["nuAl"].astype(float)
 nuYb = data_ErYb["nuYb"].astype(float)
 nuSr = data_ErYb["nuSr"].astype(float)
-
-# print("Summary of the first 100 elements of the 'MJD' column")
-# #summary(data_ErYb$MJD[1:100])
-# print("comb: ", data_ErYb['MJD'][:100].describe())
-# print("Al: ", shift_data_Al['MJD'][:100].describe())
-# print("Sr: ", shift_data_Sr['MJD'][:100].describe())
-# print("Yb: ", shift_data_Yb['MJD'][:100].describe()) 
+#convert comb nu values to high precision decimal 
+nuAl = [Decimal(i) for i in nuAl]
+nuSr = [Decimal(i) for i in nuSr]
+nuYb = [Decimal(i) for i in nuYb] 
 
 # Length of the 'MJD' column
 len_comb = len(common_mjd) 
@@ -161,13 +192,6 @@ len_Al = len(shift_data_Al['MJD'])
 len_Sr = len(shift_data_Sr['MJD'])
 len_Yb = len(shift_data_Yb['MJD'])
 
-# print("Summary of the last 100 elements of the 'MJD' column")
-# print("comb: ", data_ErYb['MJD'][(len_comb-100):].describe())
-# print("Al: ", shift_data_Al['MJD'][(len_Al-100):].describe())
-# print("Sr: ", shift_data_Al['MJD'][(len_Sr-100):].describe())
-# print("Yb: ", shift_data_Al['MJD'][(len_Yb-100):].describe())
-
-## Find intersecting indices and adjust data accordingly 
 #function to extract element as close to target as possible w/out going over
 def lb_extract(target, data):
     inx = 0
@@ -188,86 +212,53 @@ def ub_extract(target, data):
         else:
             return len(data)-inx  
 
-# Lower bound indices for each data set 
-# print(shift_data_Sr["MJD"][0])
-comb_lb_inx = lb_extract(target = shift_data_Sr["MJD"][0], data = common_mjd)
-Al_lb_inx = lb_extract(target = shift_data_Sr["MJD"][0], data = shift_data_Al["MJD"])
-Yb_lb_inx = lb_extract(target = shift_data_Sr["MJD"][0], data = shift_data_Yb["MJD"])
-# print(common_mjd[1865])
-# print(shift_data_Al["MJD"][39324])
-# print(shift_data_Yb["MJD"][1633])
+#Compare start and end points (assuming no missing data b/c IS_GOOD variable already accounted for)
+print("first comb time point: ", common_mjd[0])
+print("first good Al time point: ", shift_data_Al_good["MJD"][0])
+print("first good Sr time point: ", shift_data_Sr_good["MJD"][0])
+print("first good Yb time point: ", shift_data_Yb_good["MJD"][0])
+# print("since Sr starts the latest, start comb observations at index ", lb_extract(target = shift_data_Sr_good["MJD"][0], data = common_mjd), 
+#         ", start Al observations at index ", lb_extract(target = shift_data_Sr_good["MJD"][0], data = shift_data_Al_good["MJD"]),
+#         " and start Yb observations at index ", lb_extract(target = shift_data_Sr_good["MJD"][0], data = shift_data_Yb_good["MJD"]))
 
-# Upper bound indices for each data set 
-# print(common_mjd[len_comb-1])
-Al_ub_inx = ub_extract(target = common_mjd[len_comb-1], data = shift_data_Al["MJD"])
-Sr_ub_inx = ub_extract(target = common_mjd[len_comb-1], data = shift_data_Sr["MJD"])
-Yb_ub_inx = ub_extract(target = common_mjd[len_comb-1], data = shift_data_Yb["MJD"])
-# print(shift_data_Al["MJD"][57191])
-# print(shift_data_Sr["MJD"][1975])
-# print(shift_data_Yb["MJD"][21443])
+print("last comb time point: ", common_mjd[len_comb-1])
+print("last good Al time point: ", shift_data_Al_good["MJD"][len_Al-1])
+print("last good Sr time point: ", shift_data_Sr_good["MJD"][len_Sr-1])
+print("last good Yb time point: ", shift_data_Yb_good["MJD"][len_Yb-1])
+# print("since Al ends first, end comb observations at index ", ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = common_mjd), 
+#         ", end Sr observations at index ", ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = shift_data_Sr_good["MJD"]),
+#         " and end Yb observations at index ", ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = shift_data_Yb_good["MJD"]))
 
+
+# The following must be edited based on the resuts of the print statements above
 #comb MJD index 
-common_mjd = common_mjd[comb_lb_inx:(len_comb-1)]
-nuAl = nuAl[comb_lb_inx:(len_comb-1)]
-nuYb = nuYb[comb_lb_inx:(len_comb-1)]
-nuSr = nuSr[comb_lb_inx:(len_comb-1)]
-#convert comb nu values to high precision decimal 
-nuAl = [Decimal(i) for i in nuAl]
-nuSr = [Decimal(i) for i in nuSr]
-nuYb = [Decimal(i) for i in nuYb] 
+comb = pd.DataFrame()
+comb_init = lb_extract(target = shift_data_Sr_good["MJD"][0], data = common_mjd) #edit
+comb_end = ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = common_mjd) #edit
+comb["MJD"] = common_mjd[comb_init:comb_end]
+comb["nuAl"] = nuAl[comb_init:comb_end]
+comb["nuYb"] = nuYb[comb_init:comb_end]
+comb["nuSr"] = nuSr[comb_init:comb_end]
 
-
-#Al MJD index
-shift_data_Al = shift_data_Al[Al_lb_inx:Al_ub_inx]
-# print("Al: ", shift_data_Al["MJD"].describe())
-
-#Sr MJD index 
-shift_data_Sr = shift_data_Sr[:Sr_ub_inx]
-# print("Sr: ", shift_data_Sr["MJD"].describe())
-
-#Yb MJD index 
-shift_data_Yb = shift_data_Yb[Yb_lb_inx:Yb_ub_inx]
-# print("Yb: ", shift_data_Yb["MJD"].describe())
-
-##TODO: Implement two step approach (impute then interpolate)
-##TODO: analyse in two pieces 
-##TODO: Visualize gap frequency and sizes
-##TODO: Two step approach only for small NaN gaps  
-## Maybe: create plots of ratios (not of offset), consider ave ratio rather than ave offset? 
-##TODO: Ask for clock code for computing AVAR
-##Goal: write up NIST tech note 
-
-#shift_Sr_tmp = shift_data_Sr["shift"].interpolate(method="linear")
-#shiftSr = np.interp(data_ErYb["MJD"], shift_data_Sr["MJD"], shift_Sr_tmp)
+#Al MJD index - edit
+shift_data_Al = shift_data_Al_good[lb_extract(target = shift_data_Sr_good["MJD"][0], data = shift_data_Al_good["MJD"]):len_Al-1]
+#Sr MJD index - edit
+shift_data_Sr = shift_data_Sr_good[0:ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = shift_data_Sr_good["MJD"])]
+#Yb MJD index - edit 
+shift_data_Yb = shift_data_Yb_good[lb_extract(target = shift_data_Sr_good["MJD"][0], data = shift_data_Yb_good["MJD"]):ub_extract(target = shift_data_Al_good["MJD"][len_Al-1], data = shift_data_Yb_good["MJD"])]
 
 
 ################################################################################
 #########################  Interpolation techniques  ###########################
 ################################################################################
 
-# Method 1) Use numpy.interp to interpolate Al, Sr, and Yb shift values onto 
-#           comb MJD values 
-shiftAl1 = np.interp(common_mjd, shift_data_Al["MJD"], shift_data_Al["shift"])
-shiftSr1 = np.interp(common_mjd, shift_data_Sr["MJD"], shift_data_Sr["shift"])
-shiftYb1 = np.interp(common_mjd, shift_data_Yb["MJD"], shift_data_Yb["shift"])
+shiftAl = np.interp(common_mjd, shift_data_Al["MJD"], shift_data_Al["shift"])
+shiftSr = np.interp(common_mjd, shift_data_Sr["MJD"], shift_data_Sr["shift"])
+shiftYb = np.interp(common_mjd, shift_data_Yb["MJD"], shift_data_Yb["shift"])
 
-# change Method 1 data type to high-precision Decimal
-shiftAl1 = [Decimal(i) for i in shiftAl]
-shiftSr1 = [Decimal(i) for i in shiftSr]
-shiftYb1 = [Decimal(i) for i in shiftYb]
-
-# Method 2) Use pandas. numpy.interp to interpolate Al, Sr, and Yb shift values onto 
-#           comb MJD values 
-shift_Sr_tmp = shift_data_Sr["shift"].interpolate(method="linear")
-shiftAl1 = np.interp(common_mjd, shift_data_Al["MJD"], shift_data_Al["shift"])
-shiftSr1 = np.interp(common_mjd, shift_data_Sr["MJD"], shift_data_Sr["shift"])
-shiftYb1 = np.interp(common_mjd, shift_data_Yb["MJD"], shift_data_Yb["shift"])
-
-# change Method 1 data type to high-precision Decimal
-shiftAl1 = [Decimal(i) for i in shiftAl]
-shiftSr1 = [Decimal(i) for i in shiftSr]
-shiftYb1 = [Decimal(i) for i in shiftYb]
- 
+shiftAl = [Decimal(i) for i in shiftAl]
+shiftSr = [Decimal(i) for i in shiftSr]
+shiftYb = [Decimal(i) for i in shiftYb] 
  
 ################################################################################
 #############################  Plotting  #######################################
@@ -294,11 +285,9 @@ frequency_Yb_ErYb = [(i + j) * total_correction_Yb for i,j in zip(nuYb, shiftYb)
 frequency_Sr_ErYb = [(i + j) * total_correction_Sr for i,j in zip(nuSr, shiftSr)]
 frequency_Al_ErYb = [(i + j) * total_correction_Al for i,j in zip(nuAl, shiftAl)]
  
-
 # Yb/Sr ratio offset from BACON paper
 frequency_ratio_ErYb1 = [(i / j - YbSrRatio2020)/YbSrRatio2020 for i,j in zip(frequency_Yb_ErYb, frequency_Sr_ErYb)]
 print("Yb/Sr ratio offset from BACON paper", '{:0.5}'.format(np.nanmean(frequency_ratio_ErYb1)) )
- 
 plt.figure()
 plt.plot(common_mjd, frequency_ratio_ErYb1, '.')
 plt.grid(visible=True, which='major', color='#666666', linestyle='-')
@@ -313,7 +302,6 @@ plt.show()
 # Al/Yb ratio offset 
 frequency_ratio_ErYb2 = [(i / j - AlYbRatio2020)/AlYbRatio2020 for i,j in zip(frequency_Al_ErYb, frequency_Yb_ErYb)]
 print("Al/Yb ratio offset from BACON paper", '{:0.5}'.format(np.nanmean(frequency_ratio_ErYb2)) )
- 
 plt.figure()
 plt.plot(common_mjd, frequency_ratio_ErYb2, '.')
 plt.grid(visible=True, which='major', color='#666666', linestyle='-')
@@ -328,7 +316,6 @@ plt.show()
 # Al/Sr ratio offset  
 frequency_ratio_ErYb3 = [(i / j - AlSrRatio2020)/AlSrRatio2020 for i,j in zip(frequency_Al_ErYb, frequency_Sr_ErYb)]
 print("Al/Sr ratio offset from BACON paper", '{:0.5}'.format(np.nanmean(frequency_ratio_ErYb3)) )
- 
 plt.figure()
 plt.plot(common_mjd, frequency_ratio_ErYb3, '.')
 plt.grid(visible=True, which='major', color='#666666', linestyle='-')
