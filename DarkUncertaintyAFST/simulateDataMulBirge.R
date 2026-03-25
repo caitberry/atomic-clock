fe8b5c8e84e35531d3a27abe8ee7ce448cbaaa0d
smt3 <smt3@nist.gov>
2026-03-25 17:07:07 -0400

    create plot with MB uncertainty

--- DarkUncertaintyAFST/simulateDataMulBirge.R
+++ DarkUncertaintyAFST/simulateDataMulBirge.R
@@ -39,7 +39,7 @@ uncertainties = runif(N, min(bacon_uncertainties), max(bacon_uncertainties)) # s
 norm_gen <- function(sd_term){rnorm(1, mean=mu, sd = sd_term*birg_constant)}
 measurements = sapply(uncertainties, norm_gen)
 sim_dat = data.frame(day = 1:N, x = measurements, u = uncertainties)
-
+summary(sim_dat)
 
 std_u <- function(uncertainties){
     return(sum(1/(uncertainties^2))^(-1/2))
@@ -51,7 +51,7 @@ weighted_mean <- function(measurements, uncertainties){
     return(u_all^2 * sum(measurements/(uncertainties^2)))
 }
 
-#u(mu_hat_B)
+#u(mu_hat_B) (from Toman et al. 2012) 
 u_MB <- function(measurements, uncertainties){
     n = length(measurements)
     ave_WM = weighted_mean(measurements, uncertainties)
@@ -59,6 +59,11 @@ u_MB <- function(measurements, uncertainties){
     u = std_u(uncertainties)
     return(u*sqrt(chi_sq_obs/(n-1))) ##or use n-3 as in Toman 2012
 }
+
+## mu_hat_B and u(mu_hat_B) for simulated data
+mu_hat_b = weighted_mean(sim_dat$x, sim_dat$u)
+u_mu_hat_b = u_MB(sim_dat$x, sim_dat$u)
+u_wm = std_u(sim_dat$u) ##unadjusted uncertainty
 
 
 # plot real BACON2 data
@@ -69,11 +74,15 @@ ggplot(ratiodf, aes(x=as.character(date), y=offset)) +
   theme_bw() +
   ggtitle("BACON2 data")
 
-# plot for one simulated data set
+# plot simulated data set
 # TODO: add to this plot mu_hat with uncertainty bars showing k*u, where k=1 and plot true mu
 ggplot(sim_dat, aes(x=day, y=x)) +
   geom_point(size=1) +
   geom_errorbar(aes(ymin=x-u, ymax=x+u), width=0) +
+  geom_hline(yintercept = mu, color = "red") + 
+  geom_hline(yintercept = mu_hat_b, color = "purple") + 
+  geom_hline(yintercept = mu_hat_b + u_mu_hat_b, linetype = 2, color = "purple") + 
+    geom_hline(yintercept = mu_hat_b - u_mu_hat_b, linetype = 2, color = "purple") + 
   ylim(c(-6,6)) +
   # ylim(c(-110,-90)) + # if use mu=mean(BACON2 data)
   theme_bw() +
