a0daaa42e4572e01f696bb20f40f92eeeecb574a
smt3 <smt3@nist.gov>
2026-04-02 11:59:32 -0400

    linear pool and mul birge plot

--- DarkUncertaintyAFST/simulateDataMulBirge.R
+++ DarkUncertaintyAFST/simulateDataMulBirge.R
@@ -7,6 +7,8 @@
 ### March 2026
 ### Code adapted from Amanda Koepke and Angela Folz
 ### 
+### Assumption: each measurement x_i comes from a N(mu, c^2u_i^2) distribution
+### using Angela's technique for generating u_i values
 ############################################################################
 
 library(readr)
@@ -16,9 +18,6 @@ library(metafor)
 
 path = "/Users/smt3/Documents/GitHub/atomic-clock/"
 simdatfolder = "DarkUncertaintyAFST/simulatedData/"
-
-## Assumption: each measurement x_i comes from a N(mu, c^2u_i^2) distribution
-## use Angela's technique for generating u_i values
 
 ##BACON data 
 ratiolab = "YbSr"
@@ -31,16 +30,10 @@ N = length(bacon_measurements)
 birg_constant = 2
 mu = 0
 
-##Simulated data
-##these uncertainties are (likely) std deviations
-uncertainties = runif(N, min(bacon_uncertainties), max(bacon_uncertainties)) # simulate "known" uncertainties for each day from a uniform, 
-                                                         # with the lower and upper bound set to be what was observed 
-                                                         # in the BACON2 data for a particular ratio
+##---Supporting functions------------------------------
 norm_gen <- function(sd_term){rnorm(1, mean=mu, sd = sd_term*birg_constant)}
-measurements = sapply(uncertainties, norm_gen)
-sim_dat = data.frame(day = 1:N, x = measurements, u = uncertainties)
-summary(sim_dat)
 
+#standard uncertainty of WM
 std_u <- function(uncertainties){
     return(sum(1/(uncertainties^2))^(-1/2))
 }
@@ -52,20 +45,31 @@ weighted_mean <- function(measurements, uncertainties){
 }
 
 #u(mu_hat_B) (from Toman et al. 2012) 
-u_MB <- function(measurements, uncertainties){
-    n = length(measurements)
-    ave_WM = weighted_mean(measurements, uncertainties)
-    chi_sq_obs = sum( ((ave_WM - measurements)^2) / ((uncertainties)^2))
-    u = std_u(uncertainties)
-    return(u*sqrt(chi_sq_obs/(n-1))) ##or use n-3 as in Toman 2012
+u_MB <- function(measurements, uncertainties, correction = FALSE){
+  n = length(measurements)
+  ave_WM = weighted_mean(measurements, uncertainties)
+  chi_sq_obs = sum( ((ave_WM - measurements)^2) / ((uncertainties)^2))
+  u = std_u(uncertainties)
+  if(correction){  
+    return(u*sqrt(chi_sq_obs/(n-3))) ##n-3 is correction suggested in Toman 2012
+  } else { 
+    return(u*sqrt(chi_sq_obs/(n-1)))
+  }
 }
 
-## mu_hat_B and u(mu_hat_B) for simulated data
-mu_hat_b = weighted_mean(sim_dat$x, sim_dat$u)
-u_mu_hat_b = u_MB(sim_dat$x, sim_dat$u)
-u_wm = std_u(sim_dat$u) ##unadjusted uncertainty
+
+##---Single simulation----------------------------------
+##Simulated data
+##these uncertainties are (likely) std deviations
+uncertainties = runif(N, min(bacon_uncertainties), max(bacon_uncertainties)) # simulate "known" uncertainties for each day from a uniform, 
+                                                         # with the lower and upper bound set to be what was observed 
+                                                         # in the BACON2 data for a particular ratio
+measurements = sapply(uncertainties, norm_gen)
+sim_dat = data.frame(day = 1:N, x = measurements, u = uncertainties)
+summary(sim_dat)
 
 
+##---Plots--------------------------------------
 # plot real BACON2 data
 ggplot(ratiodf, aes(x=as.character(date), y=offset)) +
   geom_point(size=1) +
@@ -76,17 +80,82 @@ ggplot(ratiodf, aes(x=as.character(date), y=offset)) +
 
 # plot simulated data set
 # plot includes mu_hat with uncertainty bars showing k*u, where k=1 and true mu (red)
-ggplot(sim_dat, aes(x=day, y=x)) +
+p1 <- ggplot(sim_dat, aes(x=day, y=x)) +
   geom_point(size=1) +
   geom_errorbar(aes(ymin=x-u, ymax=x+u), width=0) +
-  geom_hline(yintercept = mu, color = "red") + 
-  geom_hline(yintercept = mu_hat_b, color = "purple") + 
-  geom_hline(yintercept = mu_hat_b + u_mu_hat_b, linetype = 2, color = "purple") + 
-    geom_hline(yintercept = mu_hat_b - u_mu_hat_b, linetype = 2, color = "purple") + 
-  ylim(c(-6,6)) +
+  geom_hline(aes(yintercept = mu, color = "true mu", linetype = "true mu")) + 
+  geom_hline(aes(yintercept = mu_hat_b, color = "weighted mean", linetype = "weighted mean")) + 
+  geom_hline(aes(yintercept = mu_hat_b + u_mu_hat_b, color = "unadjusted Birge bounds", linetype = "unadjusted Birge bounds")) + 
+  geom_hline(aes(yintercept = mu_hat_b - u_mu_hat_b, color = "unadjusted Birge bounds", linetype = "unadjusted Birge bounds")) +
+  geom_hline(aes(yintercept = mu_hat_b + u_mu_hat_b_corrected, color = "adjusted Birge bounds", linetype = "adjusted Birge bounds")) + 
+  geom_hline(aes(yintercept = mu_hat_b - u_mu_hat_b_corrected, color = "adjusted Birge bounds", linetype = "adjusted Birge bounds")) + 
+  # ylim(c(-6,6)) +
   # ylim(c(-110,-90)) + # if use mu=mean(BACON2 data)
+  scale_color_manual(name = "Legend",
+              breaks = c("true mu", 
+               "weighted mean", 
+               "unadjusted Birge bounds", 
+               "adjusted Birge bounds"),
+              values = c("true mu" = "red", 
+                "weighted mean" = "purple", 
+                "unadjusted Birge bounds" = "purple", 
+                "adjusted Birge bounds" = "purple"),
+              labels = c("true mu" = expression(mu), 
+               "weighted mean" = expression(hat(mu)[WM]),
+               "unadjusted Birge bounds" = "unadjusted Birge bounds",
+               "adjusted Birge bounds" = "adjusted Birge bounds")) +
+  scale_linetype_manual(name = "Legend",
+              breaks = c("true mu", 
+               "weighted mean", 
+               "unadjusted Birge bounds", 
+               "adjusted Birge bounds"),
+              values = c("true mu" = 1, 
+                "weighted mean" = 1, 
+                "unadjusted Birge bounds" = 2, 
+                "adjusted Birge bounds" = 3),
+              labels = c("true mu" = expression(mu), 
+               "weighted mean" = expression(hat(mu)[WM]),
+               "unadjusted Birge bounds" = "unadjusted Birge bounds",
+               "adjusted Birge bounds" = "adjusted Birge bounds")) +
   theme_bw() +
+  ylab(expression(x[i] %+-% u(x[i]))) + xlab("Day") + 
   ggtitle(paste("Simulated data, N =", N, ", c =", birg_constant))
 
+ggsave("DarkUncertaintyAFST/mul_birge_plot.png", plot = p1, width = 6, height = 4, units = "in", dpi = 300)
 
-##TODO: generate many data sets to investigate coverage (i'm predicting over coverage) 
+
+##---Investigate coverage------------------------------
+N_cov = 1000  ## select number of runs
+
+uncertainties_cov = runif(N*N_cov, min(bacon_uncertainties), max(bacon_uncertainties))
+measurements_cov = sapply(uncertainties_cov, norm_gen)
+day_cov = rep(1:N, N_cov)
+sim_dat_long = cbind(day_cov, measurements_cov, uncertainties_cov) 
+
+sim_dat_list = list()
+run = 1
+for(i in 1:N_cov){
+  sim_dat_list[[i]] = sim_dat_long[(run):(N+run-1), ]
+  colnames(sim_dat_list[[i]]) = c("day", "x", "u")
+  run = run + N 
+}
+
+birge_wrapper <- function(mtx){
+  mean_birge = weighted_mean(mtx[,"x"], mtx[,"u"])
+  u_birge = u_MB(mtx[,"x"], mtx[,"u"])
+  u_birge_corrected = u_MB(mtx[,"x"], mtx[,"u"], correction = TRUE)
+  return(data.frame(mean_birge = mean_birge, u_birge = u_birge, u_birge_corrected = u_birge_corrected))
+}
+birge_res = lapply(sim_dat_list, birge_wrapper)
+
+##Nominal coverage is for k=1 (68%)
+k_cov_factor = 1.96 #1.96 for 95% intervals
+coverage = 0
+coverage_corrected = 0
+for (i in 1:N_cov){
+  tmp = birge_res[[i]]
+  if((tmp$mean_birge - k_cov_factor*tmp$u_birge <= mu) & (tmp$mean_birge + k_cov_factor*tmp$u_birge >= mu)){ coverage = coverage + 1}
+  if((tmp$mean_birge - k_cov_factor*tmp$u_birge_corrected <= mu) & (tmp$mean_birge + k_cov_factor*tmp$u_birge_corrected >= mu)){ coverage_corrected = coverage_corrected + 1}
+}
+coverage/N_cov
+coverage_corrected/N_cov
