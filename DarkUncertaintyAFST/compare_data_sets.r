dce1afbf37f77ab60472db8958c47fd25e0c8b2a
smt3 <169713120+thosuz@users.noreply.github.com>
2026-07-09 15:15:48 -0400

    update compare_data_sets

--- DarkUncertaintyAFST/compare_data_sets.r
+++ DarkUncertaintyAFST/compare_data_sets.r
@@ -1,5 +1,4 @@
 ## code to create plots of simulated MB and REM data (for various c and xi values)
-## and compare to BACON data
 
 rm(list=ls())
 library(readr)
@@ -11,7 +10,7 @@ path = "/Users/smt3/Documents/GitHub/atomic-clock/"
 simdatfolder = "DarkUncertaintyAFST/simulatedData/" #added to gitignore
 figfolder = "DarkUncertaintyAFST/figures/" #added to gitignore
 
-##BACON data
+##---BACON data-------------------------------------
 ratiolab = "YbSr"
 ratiodf = read_csv(paste0(path, "Data/ClockComparison2025/BayesianAnalysisData/ErYb_",ratiolab,"_data.csv"))
 ratiodf$offset_centered = ratiodf$offset - mean(ratiodf$offset)
@@ -69,4 +68,124 @@ p_combined_tidy <- ggplot(combined_df, aes(x = x_val, y = y_val, color = source)
     color = "Data Source"
   )
 
-print(p_combined_tidy)
+df1 <- ratiodf %>% 
+    transmute(x_val = as.character(date), y_val = offset_centered, uncertainty = statistical_unc, source = "BACON2")
+df2 <- sim_dat_MB %>% 
+    transmute(x_val = as.character(ratiodf$date), y_val = x, uncertainty = u, source = "Simulated MB")
+df3 <- sim_dat_REM %>% 
+    transmute(x_val = as.character(ratiodf$date), y_val = x, uncertainty = u, source = "Simulated REM")
+
+p1 <- ggplot(df1, aes(x = x_val, y = y_val)) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = y_val - uncertainty, ymax = y_val + uncertainty), width = 0) +
+  theme_bw() +
+  ylim(-6,9) +
+  labs(
+    title = "BACON Data",
+    x = "Day / Date",
+    y = expression(x[i] %+-% u(x[i]))
+  )
+
+p2 <- ggplot(df2, aes(x = x_val, y = y_val)) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = y_val - uncertainty, ymax = y_val + uncertainty), width = 0) +
+  theme_bw() +
+  ylim(-6,9) +
+  labs(
+    title = "MB Data",
+    x = "Day / Date",
+    y = expression(x[i] %+-% u(x[i]))
+  )
+
+p3 <- ggplot(df3, aes(x = x_val, y = y_val)) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = y_val - uncertainty, ymax = y_val + uncertainty), width = 0) +
+  theme_bw() +
+  ylim(-6,9) +
+  labs(
+    title = "REM Data",
+    x = "Day / Date",
+    y = expression(x[i] %+-% u(x[i]))
+  )
+
+
+grid.arrange(p1, p2, p3, nrow = 1)
+
+
+##---Other N Simulated Data--------------------------
+
+N_new = c(5, 33, 100)
+c_vals = c(1.5, 2, 2.5)
+xi_vals = c(1, 3, 10)
+
+MB_data = REM_data = list()
+i_MB = i_REM = 1 
+
+for(N in N_new){
+  uncertainties <- runif(N, min(bacon_uncertainties), max(bacon_uncertainties))
+
+  for(c_val in c_vals){
+    measurements_MB <- sapply(uncertainties, function(sd_term){rnorm(1, mean=mu, sd = sd_term*c_val)} )
+    sim_dat_MB <- data.frame(day = 1:N, x = measurements_MB, u = uncertainties)
+
+    MB_data[[i_MB]] <- sim_dat_MB %>%
+        transmute(x_val = as.character(sim_dat_MB$day), 
+        y_val = x, 
+        uncertainty = u, 
+        source = "Simulated MB", 
+        N = N,
+        c = c_val)
+
+    i_MB = i_MB + 1
+  }
+
+  for(xi in xi_vals){
+    lambda <- rnorm(N, 0, xi)
+    epsilon <- rnorm(N, 0, uncertainties)
+
+    measurements_REM <- mu + lambda + epsilon
+    sim_dat_REM <- data.frame(day=1:N, x=measurements_REM, u=uncertainties)
+
+    REM_data[[i_REM]] <-  sim_dat_REM %>%
+        transmute(x_val = as.character(sim_dat_REM$day), 
+        y_val = x, 
+        uncertainty = u, 
+        source = "Simulated REM", 
+        N = N,
+        xi = xi)
+
+    i_REM = i_REM + 1
+  }
+}
+
+
+p_sim_MB = p_sim_REM = list()
+
+for(i in 1:9){
+  p_sim_MB[[i]] <- ggplot(MB_data[[i]], aes(x = x_val, y = y_val)) +
+    geom_point(size = 1) +
+    geom_errorbar(aes(ymin = y_val - uncertainty, ymax = y_val + uncertainty), width = 0) +
+    theme_bw() +
+    labs(
+      title = paste0("MB Data; N = ", MB_data[[i]]$N[1], "; c = ", MB_data[[i]]$c[1]),
+      x = "Day / Date",
+      y = expression(x[i] %+-% u(x[i]))
+    )
+
+  p_sim_REM[[i]] <- ggplot(REM_data[[i]], aes(x = x_val, y = y_val)) +
+    geom_point(size = 1) +
+    geom_errorbar(aes(ymin = y_val - uncertainty, ymax = y_val + uncertainty), width = 0) +
+    theme_bw() +
+    labs(
+      title = paste0("REM Data; N = ", REM_data[[i]]$N[1], "; xi = ", REM_data[[i]]$xi[1]),
+      x = "Day / Date",
+      y = expression(x[i] %+-% u(x[i]))
+    )
+}
+
+
+for (i in c(1,4,7)){
+  combined_plot <- arrangeGrob(p_sim_MB[[i]], p_sim_MB[[i+1]], p_sim_MB[[i+2]],
+                    p_sim_REM[[i]], p_sim_REM[[i+1]], p_sim_REM[[i+2]], nrow = 2)
+  ggsave(paste0("DarkUncertaintyAFST/figures/sim_data_comparison_N", REM_data[[i]]$N[1], ".png"), combined_plot, device = "png")
+}
